fab1ffd7706a048f6c01bdcd3ac0b22ca9d06043
Eric Farley <eric@ericfarley.net>
2018-01-07 00:29:39 -0800

    adds material support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialsModule } from './materials.module';
 
 
--- src/app/materials.module.ts
+++ src/app/materials.module.ts
@@ -2,11 +2,11 @@ import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [
-        MatInputModule
-    ],
-    exports: [
-        MatInputModule
-    ]
+  imports: [
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
+  ]
 })
 export class MaterialsModule { }
