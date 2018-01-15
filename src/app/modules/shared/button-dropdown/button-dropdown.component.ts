0a601894449847a23ae3eecbd2a4fb23caa63c91
Eric Farley <eric@ericfarley.net>
2018-01-14 21:25:12 -0800

    Adds split to dropdown component

--- src/app/modules/shared/button-dropdown/button-dropdown.component.ts
+++ src/app/modules/shared/button-dropdown/button-dropdown.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, transition, style, state, trigger, animate, ElementRef } from '@angular/core';
+import { AppColors } from '../../../enums/app-colors.enum';
 
 @Component({
   selector: 'kr-button-dropdown',
   templateUrl: './button-dropdown.component.html',
   styleUrls: ['./button-dropdown.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('expand', [
+      state('0', style({
+        display: 'none',
+        height: '0px'
+      })),
+      state('1', style({
+        display: 'inline-flex',
+        height: '*'
+      })),
+      transition('* => *', animate('300ms ease-in-out')),
+      transition('void => *', animate(0))
+    ])
+  ]
 })
 export class ButtonDropdownComponent implements OnInit {
+  @Input() color: AppColors;
+  private isExpanded = false;
   @Input() label: string;
 
-  constructor() { }
+  constructor(
+    private element: ElementRef
+  ) { }
 
   ngOnInit() {
+  }
+
+  private get isSplit(): boolean {
+   return this.element.nativeElement.hasAttribute('split');
   }
 
 }
